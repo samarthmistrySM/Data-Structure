907319a3de8771ec63c5cc91650e6bd391ed5f80
Samarth <11346030-samarthmistry31@users.noreply.replit.com>
2024-12-12 05:07:46 +0000

    Samarth Add: StackUsingClass which is full implementation of Datastucture of stack using array

---
+++ StackUsingClass.js
@@ -0,0 +1,35 @@
+class Stack {
+  constructor() {
+    this.items = []; 
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.join(' '));
+  }
+}
