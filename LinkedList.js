5bab36a75f112ac5886e8202befad5d4cf33da46
samarthmistrySM <g33ky.sam@gmail.com>
2024-12-18 14:40:50 +0530

    Samarth Add: Implemented LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -31,18 +31,18 @@ class Linkedlist {
   }
 
   addAt(data, index) {
-    if(index > this.size){
+    if (index > this.size) {
       throw new Error("Index out of Bound");
     }
     const newNode = new Node(data);
-    
-    if (this.head === 0) {
+
+    if (this.head === null) {
       newNode.next = this.head;
     } else {
       let curr = this.head;
       let currIndex = 0;
       let temp = null;
-      while(currIndex <index){
+      while (currIndex < index) {
         temp = curr;
         curr = curr.next;
         currIndex++;
@@ -52,6 +52,100 @@ class Linkedlist {
     }
     this.size++;
   }
+
+  deleteAt(index) {
+    if (index > this.size) {
+      throw new Error("Index out of Bound");
+    }
+    if (this.head === null) {
+      throw new Error("Linkedlist is Empty");
+    } else {
+      let curr = this.head;
+      let currIndex = 0;
+      let prev = null;
+      while (currIndex < index) {
+        prev = curr;
+        curr = curr.next;
+        currIndex++;
+      }
+      prev.next = curr.next;
+      this.size--;
+    }
+  }
+
+  removeFirstOcc(data) {
+    if (this.head === null) {
+      throw new Error("Linkedlist is Empty");
+    } else {
+      let curr = this.head;
+      let prev = null;
+      while (curr.data != data) {
+        prev = curr;
+        curr = curr.next;
+      }
+      prev.next = curr.next;
+    }
+    this.size--;
+  }
+
+  removeAll(data) {
+    if (this.head === null) {
+      throw new Error("Linkedlist is Empty");
+    } else {
+      let curr = this.head;
+      let prev = null;
+      
+      while (curr != null) {
+        if(curr.data === data){
+          prev.next = curr.next;
+          this.size--;
+        }
+        else{
+          prev = curr;
+        }
+        curr = curr.next; 
+      }
+    }
+  }
+
+  contains(data){
+    let curr = this.head;
+    while (curr != null) {
+      if(curr.data === data){
+        return true;
+      }
+      curr = curr.next;
+    }
+    return false;
+  }
+
+  addFirst(data){
+    const newNode = new Node(data);
+    if(this.head === null){
+      this.head = newNode;
+    }else{
+      const temp = this.head;
+      newNode.next = temp;
+      this.head = newNode;
+    }
+  }
+  
+  indexOf(data){
+    let found = false;
+    let currIndex = 0;
+    let curr = this.head;
+    while(curr.data != data){
+      curr = curr.next;
+      currIndex++;
+      found=true;
+    }
+    if(!found){
+      currIndex = -1;
+    }
+    return currIndex;
+  }
+
+
 }
 
-module.exports = Linkedlist;
+module.exports = Linkedlist
--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const linkedlist = new Linkedlist();
 linkedlist.add(1);
 linkedlist.add(2);
 linkedlist.add(3);
+linkedlist.add(3);
 linkedlist.add(4);
 
 linkedlist.print()
-
-linkedlist.addAt(5,3);
-
+linkedlist.removeAll(3);
 linkedlist.print()
+// linkedlist.addAt(5,3);
+
+// linkedlist.print()
+
+// linkedlist.deleteAt(3);
+// linkedlist.removeFirstOcc(2);
+
+// linkedlist.print()
+
+// log(linkedlist.contains(2))
+
+// linkedlist.addFirst(0)
+// linkedlist.print()
+
+// console.log(linkedlist.indexOf(3))
