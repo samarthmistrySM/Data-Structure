32f33b86856afa172a9a8748f5a79e79f528bc8b
samarthmistrySM <g33ky.sam@gmail.com>
2024-12-24 09:25:16 +0530

    Samarth Add: Solved some Questions based on Tree

--- BST.js
+++ BST.js
@@ -106,6 +106,109 @@ class BST {
       return this.search(root.right, data);
     } else return root;
   }
+
+  height(root=this.root){
+    if(root === null){
+        return -1;
+    }
+
+    let leftHeight = this.height(root.left);
+    let rightHeight = this.height(root.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  isIdentical(r2,r1=this.root){
+
+    if(r1===null && r2 === null){
+        return true;
+    }
+
+    if(r1===null || r2 === null){
+        return false;
+    }
+
+    if(r1.data===r2.data && this.isIdentical(r2.left,r1.left) && this.isIdentical(r2.right,r1.right)){
+        return true;
+    }else{
+        return false;
+    }
+
+  }
+
+  mirrorTree(root = this.root){
+
+    if(root === null){
+        return null;
+    }
+    const left = this.mirrorTree(root.left)
+    const right = this.mirrorTree(root.right)
+
+    root.left = right;
+    root.right = left;
+
+    return root;
+  }
+
+  isMirror(left,right){
+    if (left === null && right === null) {
+        return true;
+    }
+
+
+    if (left === null || right === null) {
+        return false;
+    }
+
+    if(left.data === right.data && this.isMirror(left.left,right.right) && this.isMirror(left.left,right.right) ){
+        return true;
+    }else{
+        return false;
+    }
+  }
+
+  isSymmetric(root=this.root){
+    if(root == null){
+        return true;
+    }else{
+        return this.isMirror(root.left,root.right);
+    }
+  }
+
+  isSum(root = this.root){
+    if (root === null || (root.left === null && root.right === null)){
+        return 1;
+    }
+
+    let sum = 0;
+    if (root.left !== null){
+        sum += root.left.data;
+    }
+
+    if (root.right !== null){
+        sum += root.right.data;
+    }
+
+    if(sum === root.data && this.isSum(root.left) && this.isSum(root.right)){
+        return true;
+    }else{
+        return false;
+    }
+  }
+
+  arrToTree(arr, start, end){
+    if(start > end){
+        return null;
+    }
+
+    let mid = Math.floor((start + end) / 2);
+    const node = new Node(arr[mid]);
+
+    node.left = this.arrToTree(arr, start, mid - 1);
+    node.right = this.arrToTree(arr, mid + 1, end);
+
+    return node;
+  }
 }
 
 // Helper function
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const BST = require("./BST")
+const Node = require("./Node")
 
 const bst = new BST();
+// const bst2 = new BST();
 
 bst.insert(40);
 bst.insert(20);
@@ -9,6 +11,14 @@ bst.insert(10);
 bst.insert(30);
 bst.insert(50);
 bst.insert(70);
+
+// bst2.insert(40);
+// bst2.insert(20);
+// bst2.insert(60);
+// bst2.insert(10);
+// bst2.insert(30);
+// bst2.insert(50);
+// bst2.insert(70);
 
 
 // bst.preOrder(bst.root)
@@ -20,5 +30,30 @@ bst.insert(70);
 
 // bst.preOrder(bst.root)
 
-const find = bst.search(10);
-console.log(typeof find);
+// const find = bst.search();
+// const height = bst.height(bst.root);
+// console.log(height);
+
+// const isIdentical = bst.isIdentical(bst2.root)
+
+// console.log(isIdentical)
+
+// bst.preOrder(bst.root)
+
+// console.log();
+
+// const mirror = bst.mirrorTree();
+
+// bst.preOrder(mirror)
+
+// console.log(bst.isSymmetric(bst.root));
+// console.log(bst.isSum(bst.root))
+
+const arrToTree = () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const bst = new BST();
+    const done = bst.arrToTree(arr, 0, arr.length - 1);
+    bst.preOrder(done)
+}
+
+arrToTree();
